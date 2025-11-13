6308b49c77211294696c69f27968589dbcc4a5a3
Yash <contact@yashgoyal.dev>
2025-11-13 15:15:29 -0300

    updated the existing useDomainNameForAddress function to show the ens address also on bnb chain and updated the chainid for the ens testnet to sepolia from goerli

--- apps/web/src/hooks/useDomain.ts
+++ apps/web/src/hooks/useDomain.ts
@@ -15,16 +15,16 @@ export const useDomainNameForAddress = (address?: `0x${string}` | string, fetchD
   )
   const { data: ensName, isLoading: isEnsLoading } = useEnsName({
     address: address as Address,
-    chainId: chainId === ChainId.GOERLI ? ChainId.GOERLI : ChainId.ETHEREUM,
+    chainId: chainId === ChainId.SEPOLIA ? ChainId.SEPOLIA : ChainId.ETHEREUM,
     query: {
-      enabled: chainId !== ChainId.BSC && chainId !== ChainId.BSC_TESTNET,
+      enabled: true,
     },
   })
   const { data: ensAvatar, isLoading: isEnsAvatarLoading } = useEnsAvatar({
     name: ensName as string,
-    chainId: chainId === ChainId.GOERLI ? ChainId.GOERLI : ChainId.ETHEREUM,
+    chainId: chainId === ChainId.SEPOLIA ? ChainId.SEPOLIA : ChainId.ETHEREUM,
     query: {
-      enabled: chainId !== ChainId.BSC && chainId !== ChainId.BSC_TESTNET,
+      enabled: true,
     },
   })
 
